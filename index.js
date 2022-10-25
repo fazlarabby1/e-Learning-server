0226fce7cd65e670730cc1bee88bd251ab36bb64
fazlarabby1 <fazlarabby41@gmail.com>
2022-10-26 02:56:06 +0600

    created the route for all courses data

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const courses = require('./data/courses.json');
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('Welcome to my fake learning CSE DB');
+})
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
 })
 
 app.listen(port, ()=>{
