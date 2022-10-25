4d2fbbc649c903e9ec82e1d4dd76b3ecb474e26d
fazlarabby1 <fazlarabby41@gmail.com>
2022-10-26 03:01:48 +0600

    created dynamic route according to courses id

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/courses', (req, res)=>{
     res.send(courses);
 })
 
+app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id == id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, ()=>{
     console.log(`The Fake learning DB is running on port ${port}`);
 })
